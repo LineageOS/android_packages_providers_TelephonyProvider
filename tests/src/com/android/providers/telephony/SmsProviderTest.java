cd859221a5ff0878ccbd81927a48a29f8455ae72
Aishwarya Mallampati <amallampati@google.com>
2023-08-23 18:30:46 +0000

    DO NOT MERGE Block access to sms/mms db from work profile.
    Bug: 289242655
    Test: Manually verified work profile cannot access personal sms by
    following steps mentioned in b/289242655#comment26
    - atest SmsProviderTest
    - atest MmsProviderTest
    - atest SmsBackupRestoreTest
    - QA performed regression testing and confirmed fix is working as intended here: b/294459052#comment30
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:950a7e5a4bf1b38e846fe00642105479efded57d)
    Merged-In: Ib1c9ec75f77e8412b53df50f5414caa0e5aaa277
    Change-Id: Ib1c9ec75f77e8412b53df50f5414caa0e5aaa277
    
    Change-Id: Ic6aca29053bf3208f5b9e5573293bb9e17103776

--- tests/src/com/android/providers/telephony/SmsProviderTest.java
+++ tests/src/com/android/providers/telephony/SmsProviderTest.java
@@ -16,6 +16,9 @@
 
 package com.android.providers.telephony;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
 import android.app.AppOpsManager;
 import android.content.ContentValues;
 import android.content.Context;
@@ -24,6 +27,7 @@ import android.content.pm.ProviderInfo;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.net.Uri;
+import android.os.UserManager;
 import android.provider.Telephony;
 import android.telephony.TelephonyManager;
 import android.test.mock.MockContentResolver;
@@ -55,6 +59,7 @@ public class SmsProviderTest extends TestCase {
     private MockContextWithProvider mContext;
     private MockContentResolver mContentResolver;
     private SmsProviderTestable mSmsProviderTestable;
+    private UserManager mUserManager;
 
     private int notifyChangeCount;
 
@@ -99,6 +104,8 @@ public class SmsProviderTest extends TestCase {
                     return Mockito.mock(AppOpsManager.class);
                 case Context.TELEPHONY_SERVICE:
                     return Mockito.mock(TelephonyManager.class);
+                case Context.USER_SERVICE:
+                    return mUserManager;
                 default:
                     return null;
             }
@@ -132,6 +139,8 @@ public class SmsProviderTest extends TestCase {
         mSmsProviderTestable = new SmsProviderTestable();
         mContext = new MockContextWithProvider(mSmsProviderTestable);
         mContentResolver = mContext.getContentResolver();
+        mUserManager = Mockito.mock(UserManager.class);
+
         notifyChangeCount = 0;
     }
 
@@ -169,5 +178,50 @@ public class SmsProviderTest extends TestCase {
         Log.d(TAG, "testInsertAttachmentTable Inserting contentValues: " + values);
         assertEquals(Uri.parse("content://sms/attachments/1"),
                 mContentResolver.insert(Uri.parse("content://sms/attachments"), values));
+    }
+
+    @Test
+    public void testInsertUsingManagedProfile() {
+        when(mUserManager.isManagedProfile(anyInt())).thenReturn(true);
+
+        try {
+            assertNull(mContentResolver.insert(Telephony.Sms.CONTENT_URI, null));
+        } catch (Exception e) {
+            Log.d(TAG, "Error inserting sms: " + e);
+        }
+    }
+
+    @Test
+    public void testQueryUsingManagedProfile() {
+        when(mUserManager.isManagedProfile(anyInt())).thenReturn(true);
+
+        try (Cursor cursor = mContentResolver.query(Telephony.Sms.CONTENT_URI,
+                null, null, null, null)) {
+            assertEquals(0, cursor.getCount());
+        } catch (Exception e) {
+            Log.d(TAG, "Exception in getting count: " + e);
+        }
+    }
+
+    @Test
+    public void testUpdateUsingManagedProfile() {
+        when(mUserManager.isManagedProfile(anyInt())).thenReturn(true);
+
+        try {
+            assertEquals(0, mContentResolver.update(Telephony.Sms.CONTENT_URI, null, null, null));
+        } catch (Exception e) {
+            Log.d(TAG, "Exception in updating sms: " + e);
+        }
+    }
+
+    @Test
+    public void testDeleteUsingManagedProfile() {
+        when(mUserManager.isManagedProfile(anyInt())).thenReturn(true);
+
+        try {
+            assertEquals(0, mContentResolver.delete(Telephony.Sms.CONTENT_URI, null, null));
+        } catch (Exception e) {
+            Log.d(TAG, "Exception in deleting sms: " + e);
+        }
     }
 }
